d98f06b0d55554d79eac25878502d2fbd3477151
Regis Antich <regis.rs@outlook.com>
2021-06-09 12:28:51 -0300

    Initial Commit

---
+++ OCRegis/OCRegisApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRegisApp.swift
+//  OCRegis
+//
+//  Created by Admin on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct OCRegisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
