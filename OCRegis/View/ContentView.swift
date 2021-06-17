e8637032a8779fceb1d02ddf274bb345bcd33500
Regis Antich <regis.rs@outlook.com>
2021-06-17 20:55:09 -0300

    Added past image

--- OCRegis/View/ContentView.swift
+++ OCRegis/View/ContentView.swift
@@ -11,10 +11,62 @@ struct ContentView: View {
     @State var image = NSImage(named: "Image-1")
     @State var dragOver = false
     @State var textoExtraido: Array<String> = []
+    
+    func pasteboardImage() -> NSImage?
+    {
+        let pb = NSPasteboard.general
+        let type = NSPasteboard.PasteboardType.tiff
+        guard let imgData = pb.data(forType: type) else { return nil }
+        
+        return NSImage(data: imgData)
+    }
+    
+    func processaImagem (imagem: NSImage?) {
+        DispatchQueue.main.async {
+            self.image = imagem
+            textoExtraido = []
+            let request = VNRecognizeTextRequest { request, error in
+                guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                    fatalError("Received invalid observations")
+                }
+                
+                for observation in observations {
+                    guard let bestCandidate = observation.topCandidates(1).first else {
+                        print("No candidate")
+                        continue
+                    }
+                    textoExtraido.append(bestCandidate.string)
+                }
+            }
+            
+            let requests = [request]
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                guard let img = image?.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+                    fatalError("Missing image to scan")
+                }
+                
+                let handler = VNImageRequestHandler(cgImage: img, options: [:])
+                try? handler.perform(requests)
+            }
+        }
+    }
+    
+    
+    
     var body: some View {
         HSplitView {
             List() {
                 Section(header: Text("Imagem")) {
+                    
+                    Button(action: {
+                        image = pasteboardImage()
+                        processaImagem(imagem: image)
+                    }) {
+                        Text("Executar")
+                    }
+                    .keyboardShortcut("v", modifiers: .command)
+                    
                     Image(nsImage: image ?? NSImage())
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -23,34 +75,8 @@ struct ContentView: View {
                             provider.first?.loadDataRepresentation(forTypeIdentifier: "public.file-url", completionHandler: { data, error in
                                 if let data = data, let path = NSString(data: data, encoding: 4), let url = URL(string: path as String) {
                                     let image = NSImage(contentsOf: url)
-                                    DispatchQueue.main.async {
-                                        self.image = image
-                                        textoExtraido = []
-                                        let request = VNRecognizeTextRequest { request, error in
-                                            guard let observations = request.results as? [VNRecognizedTextObservation] else {
-                                                fatalError("Received invalid observations")
-                                            }
-
-                                            for observation in observations {
-                                                guard let bestCandidate = observation.topCandidates(1).first else {
-                                                    print("No candidate")
-                                                    continue
-                                                }
-                                                textoExtraido.append(bestCandidate.string)
-                                            }
-                                        }
-                                        
-                                        let requests = [request]
-
-                                        DispatchQueue.global(qos: .userInitiated).async {
-                                            guard let img = image?.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
-                                                fatalError("Missing image to scan")
-                                            }
-
-                                            let handler = VNImageRequestHandler(cgImage: img, options: [:])
-                                            try? handler.perform(requests)
-                                        }
-                                    }
+                                    processaImagem(imagem: image)
+                                    
                                 }
                             })
                             return true
